43157188a27e32a036640941691c2d7a88b8263d
Jan Mikulka <jan.mikulka@email.cz>
2016-06-24 12:15:57 +0200

    Implement actions

---
+++ resources/assets/js/actions/commonActions.js
@@ -0,0 +1,7 @@
+export function startLoadingData() {
+  return { type: 'START_LOADING_DATA'};
+}
+
+export function finishLoadingData(message) {
+  return { type: 'FINISH_LOADING_DATA', message};
+}
---
+++ resources/assets/js/actions/invoiceActions.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import {REST_URL} from '../constants';
+import * as common from './commonActions';
+
+export function loadInvoicesSuccess(invoices) {
+  return function(dispatch) {
+  dispatch(common.finishLoadingData('Invoices successfully loaded'));
+  dispatch({ type: 'LOAD_INVOICES_SUCCESS', invoices});
+  }
+}
+
+export function createInvoiceSuccess(invoice) {
+  return {type: 'CREATE_INVOICE_SUCCESS', invoice};
+}
+
+export function updateInvoiceSuccess(invoice) {
+  return {type: 'UPDATE_INVOICE_SUCCESS', invoice};
+}
+
+export function loadInvoices() {
+  return function(dispatch) {
+    dispatch(common.startLoadingData());
+    return axios.get(`${REST_URL}/todos`).then(invoices => {
+      dispatch(loadInvoicesSuccess(invoices.data));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
