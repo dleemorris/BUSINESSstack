562f1947912ec480543a2d39b9dac9d61f467440
Marek Stopka <marek.stopka@perlur.cz>
2016-07-18 00:15:29 +0200

    Add PurchaseInvoice model and migration

---
+++ database/migrations/2016_07_17_221522_create_purchase_invoices_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePurchaseInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchase_invoices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('purchase_invoices');
+    }
+}
